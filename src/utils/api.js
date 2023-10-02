3236e7446776b8ecde36a3c69043b51baaba249c
SONU SINGH <sonusingh82342@gmail.com>
2023-10-03 01:37:33 +0530

    new-commit

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+           "6641e252demshc420d8a93a534e7p1ee6e0jsnec6ef43d6630",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
